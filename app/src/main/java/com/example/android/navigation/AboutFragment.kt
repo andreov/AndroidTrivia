f2f9ad80c50e8d436350a3a4a1c327d6c81e2b72
AndreyOvch <amo-rzn@ya.ru>
2022-02-01 16:01:19 +0300

    Add databinding About and Rulles Fragment. Del branch HomeWorks

--- app/src/main/java/com/example/android/navigation/AboutFragment.kt
+++ app/src/main/java/com/example/android/navigation/AboutFragment.kt
@@ -21,11 +21,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.android.navigation.databinding.FragmentAboutBinding
+
 
 class AboutFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        val binding: FragmentAboutBinding = DataBindingUtil
+            .inflate(inflater, R.layout.fragment_about, container, false)
+        return binding.root
     }
 }
--- app/src/main/java/com/example/android/navigation/RulesFragment.kt
+++ app/src/main/java/com/example/android/navigation/RulesFragment.kt
@@ -21,11 +21,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.android.navigation.databinding.FragmentAboutBinding
+import com.example.android.navigation.databinding.FragmentRulesBinding
 
 class RulesFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rules, container, false)
+        val binding: FragmentRulesBinding = DataBindingUtil
+            .inflate(inflater, R.layout.fragment_rules, container, false)
+        return binding.root
     }
 }
